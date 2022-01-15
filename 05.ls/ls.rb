2cba314d7646d99daa94ac4b8a54a10ef7a57759
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2022-01-15 21:32:20 +0900

    変数名の変更と配列に同じ要素が存在する場合でも対応できるように修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,30 +1,31 @@
 # frozen_string_literal: true
 
-NUM_COLUMN = 3
+COLUMN_SIZE = 3
+SPACE_WIDTH = 24
 
 def main
-  files_count.map do |files|
-    files.each do |file|
-      if file == files.last
-        puts "#{file}\n"
-      else
-        print file.ljust(24)
-      end
+  files_count.each do |files|
+    remove_nil_files = files.compact
+    line = remove_nil_files.sum('') do |file|
+      file.ljust(SPACE_WIDTH)
     end
+    print line.rstrip
+    print "\n"
   end
 end
 
-def files_retrieve
+def find_files
   Dir.glob('*')
 end
 
 def files_count
-  file_count_num = (files_retrieve.size / NUM_COLUMN.to_f).ceil(0)
-  file_element_segmentation = files_retrieve.each_slice(file_count_num).to_a
-  if file_element_segmentation.count == 1
-    file_element_segmentation
+  files = find_files
+  division_linecount_count_file = (files.size / COLUMN_SIZE.to_f).ceil(0)
+  segment_file = files.each_slice(division_linecount_count_file).to_a
+  if segment_file.count == 1
+    segment_file
   else
-    file_element_segmentation.reduce(&:zip).map(&:flatten)
+    segment_file.inject(&:zip).map(&:flatten)
   end
 end
 
