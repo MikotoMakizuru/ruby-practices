29becc3f50d283297ce2e7b9990b8703190c1598
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2022-02-16 00:46:02 +0900

    変数名/メソッド名の再考&リファクタリング

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -27,36 +27,20 @@ PERMISSION = {
   '7' => 'rwx'
 }.freeze
 
-def main
-  options = parse_options
-  if options.key?(:l)
-    block_size, files = retrieve_files_info
+def main(options)
+  flags = options['a'] ? File::FNM_DOTMATCH : 0
+  file_names = Dir.glob('*', flags)
+  options['r'] ? file_names.reverse! : file_names
+
+  if options['l']
+    block_size, files = retrieve_files_info(file_names)
     display_files_info(block_size, files)
   else
-    display_files
+    display_files(file_names)
   end
 end
 
-def parse_options
-  opt = OptionParser.new
-  params = {}
-  opt.on('-a') { |v| params[:a] = v }
-  opt.on('-r') { |v| params[:r] = v }
-  opt.on('-l') { |v| params[:l] = v }
-  opt.parse(ARGV)
-  params
-end
-
-def find_files
-  options = parse_options
-  flags = options.key?(:a) ? File::FNM_DOTMATCH : 0
-  file_names = Dir.glob('*', flags)
-  file_names = file_names.reverse if options.key?(:r)
-  file_names
-end
-
-def retrieve_files_info
-  file_names = find_files
+def retrieve_files_info(file_names)
   block_size = 0
   files = Hash.new { |hash, key| hash[key] = {} }
   file_names.each do |name|
@@ -64,32 +48,27 @@ def retrieve_files_info
 
     block_size += file_stat.blocks
     files[name][:type] = FILE_TYPE[File.ftype(name)]
-    files[name][:permission] = permission(file_stat)
-    files[name][:hard_link] = file_stat.nlink.to_s
-    files[name][:user] = Etc.getpwuid(File.stat(name).uid).name
-    files[name][:group] = Etc.getgrgid(File.stat(name).gid).name
-    files[name][:size] = file_stat.size.to_s
-    files[name][:month] = file_stat.mtime.month.to_s
-    files[name][:day] = file_stat.mtime.day.to_s
-    files[name][:time] = file_stat.mtime.strftime('%H:%M')
-    files[name][:file_name] = find_link_target(name)
+    files[name][:permission] = retrieve_permission(file_stat)
+    files[name][:hard_link] = file_stat.nlink
+    files[name][:user] = Etc.getpwuid(file_stat.uid).name
+    files[name][:group] = Etc.getgrgid(file_stat.gid).name
+    files[name][:size] = file_stat.size
+    files[name][:mtime] = file_stat.mtime
+    files[name][:file_name] = file_symlink_whether(name)
   end
   [block_size, files]
 end
 
-def permission(file_stat)
-  permissions = ''
+def retrieve_permission(file_stat)
   mode_octal_number = file_stat.mode.to_s(8)
-  3.times do |i|
-    permissions += PERMISSION[mode_octal_number[-3 + i]]
-  end
+  permissions = -3.upto(-1).map { |i| PERMISSION[mode_octal_number[i]] }.join
   permissions[2] = (permissions[2] == 'x' ? 's' : 'S') if file_stat.setuid?
   permissions[5] = (permissions[5] == 'x' ? 's' : 'S') if file_stat.setgid?
   permissions[8] = (permissions[8] == 'x' ? 't' : 'T') if file_stat.sticky?
   permissions
 end
 
-def find_link_target(name)
+def file_symlink_whether(name)
   File.lstat(name).symlink? ? "#{name} -> #{File.readlink(name)}" : name
 end
 
@@ -98,34 +77,30 @@ def display_files_info(block_size, files)
   files.each_value do |file|
     print file[:type]
     print file[:permission]
-    print file[:hard_link].rjust(4)
+    print file[:hard_link].to_s.rjust(4)
     print file[:user].rjust(7)
     print file[:group].rjust(7)
-    print file[:size].rjust(6)
-    print file[:month].rjust(3)
-    print file[:day].rjust(3)
-    print file[:time].rjust(6)
+    print file[:size].to_s.rjust(6)
+    print file[:mtime].month.to_s.rjust(3)
+    print file[:mtime].day.to_s.rjust(3)
+    print file[:mtime].strftime('%H:%M').to_s.rjust(6)
     print " #{file[:file_name]}"
     print "\n"
   end
 end
 
-def divide_files
-  file_names = find_files
-  return [file_names] if file_names.size == 1
-
+def display_files(file_names)
   row_count = (file_names.size / COLUMN_SIZE.to_f).ceil
-  segment_files = file_names.each_slice(row_count).to_a
-  segment_files.inject(&:zip).map(&:flatten)
-end
-
-def display_files
-  divide_files.each do |files|
-    line = files.compact.sum('') do |file|
-      file.ljust(SPACE_WIDTH)
+  divide_files = file_names.each_slice(row_count).to_a
+  # injectは要素が1つ（ファイル数が1つ）の場合、ブロックを実行せずに要素を返すため
+  divided_files = file_names.size == 1 ? divide_files : divide_files.inject(&:zip).map(&:flatten)
+  divided_files.each do |files|
+    line = files.sum('') do |file|
+      file.to_s.ljust(SPACE_WIDTH)
     end
     puts line.rstrip
   end
 end
 
-main
+options = ARGV.getopts('alr')
+main(options)
