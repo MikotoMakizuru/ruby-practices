8c443e8e8adfb5c906381d47aca6107facad06b6
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-02-28 03:03:39 +0900

    initializeの引数名修正
    スペアを判定するメソッドに「かつ1投目が10でなければ」という条件を追加

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -3,10 +3,10 @@
 require_relative 'shot'
 
 class Frame
-  def initialize(frame)
-    @first_shot = Shot.new(frame[0])
-    @second_shot = Shot.new(frame[1])
-    @third_shot = Shot.new(frame[2])
+  def initialize(marks)
+    @first_shot = Shot.new(marks[0])
+    @second_shot = Shot.new(marks[1])
+    @third_shot = Shot.new(marks[2])
   end
 
   def calculate_frame_score
@@ -18,6 +18,6 @@ class Frame
   end
 
   def spare?
-    [@first_shot.score, @second_shot.score].sum == 10
+    [@first_shot.score, @second_shot.score].sum == 10 && @first_shot.score != 10
   end
 end
