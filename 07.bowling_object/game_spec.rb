07e6d0bea3c7be4342f8e84462245d21918ad165
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-02-27 20:37:18 +0900

    適切なスコアが返ることをテストするテストコードを増加（rubocopからの指摘有）

--- 07.bowling_object/game_spec.rb
+++ 07.bowling_object/game_spec.rb
@@ -8,6 +8,27 @@ describe Game do
       example 'スコアを返すこと' do
         game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
         expect(game.score).to eq(139)
+
+        game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
+        expect(game.score).to eq(164)
+
+        game = Game.new('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
+        expect(game.score).to eq(107)
+
+        game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
+        expect(game.score).to eq(134)
+
+        game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8')
+        expect(game.score).to eq(144)
+
+        game = Game.new('X,X,X,X,X,X,X,X,X,X,X,X')
+        expect(game.score).to eq(300)
+
+        game = Game.new('X,X,X,X,X,X,X,X,X,X,X,2')
+        expect(game.score).to eq(292)
+
+        game = Game.new('X,0,0,X,0,0,X,0,0,X,0,0,X,0,0')
+        expect(game.score).to eq(50)
       end
     end
   end
