392c029c642262ad8cd1f7b66908d969ac1d7e89
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-03-16 21:34:24 +0900

    メソッド名修正

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -16,8 +16,10 @@ class Game
     game_score
   end
 
+  private
+
   def create_frames
-    source_frames = parse_input_with_three_values(parse_inputs)
+    source_frames = parse_inputs
     frames = []
     source_frames.each do |source_frame|
       frame = Frame.new(source_frame)
@@ -26,48 +28,12 @@ class Game
     frames
   end
 
-  def parse_input_with_three_values(parse_inputs)
-    parsed_inputs = parse_inputs
-    source_frames = []
-
-    9.times do |idx|
-      source_frame = parsed_inputs[idx]
-      source_next_frame = parsed_inputs[idx + 1]
-      source_next_after_frame = parsed_inputs[idx + 2]
-
-      if all_x_values?(source_frame)
-        concat_source_frame(source_frame, source_next_frame, source_next_after_frame)
-      elsif source_frame.size < 3
-        source_frame << source_next_frame[0] if source_next_frame
-      end
-
-      source_frames << if source_frame.size > 3
-                         source_frame[0..2]
-                       else
-                         source_frame
-                       end
-    end
-    source_frames << parsed_inputs.last if source_frames.size < 10
-  end
-
-  def concat_source_frame(source_frame, source_next_frame, source_next_after_frame)
-    if all_x_values?(source_next_frame)
-      source_frame.concat(source_next_frame).concat(source_next_after_frame)
-    else
-      source_frame.concat(source_next_frame)
-    end
-  end
-
-  def all_x_values?(source_frame)
-    source_frame.one? && source_frame.first == 'X'
-  end
-
-  private
-
+  # 入力された得点を1つの配列が3つの要素を持つは次元配列にparseする
   def parse_inputs
     inputs = @input.split(',')
-    source_frame = []
     source_frames = []
+    source_frame = []
+
     inputs.each do |input|
       source_frame << input
 
@@ -80,6 +46,41 @@ class Game
         source_frames.last << input
       end
     end
-    source_frames
+    source_frames_with_three_values(source_frames)
   end
+
+  def source_frames_with_three_values(parsed_inputs)
+      source_frames = []
+
+      9.times do |idx|
+        source_frame = parsed_inputs[idx]
+        source_next_frame = parsed_inputs[idx + 1]
+        source_next_after_frame = parsed_inputs[idx + 2]
+  
+        if all_x_values?(source_frame)
+          concat_source_frame(source_frame, source_next_frame, source_next_after_frame)
+        elsif source_frame.size < 3
+          source_frame << source_next_frame[0] if source_next_frame
+        end
+  
+        source_frames << if source_frame.size > 3
+                           source_frame[0..2]
+                         else
+                           source_frame
+                         end
+      end
+      source_frames << parsed_inputs.last if source_frames.size < 10
+  end
+  
+    def concat_source_frame(source_frame, source_next_frame, source_next_after_frame)
+      if all_x_values?(source_next_frame)
+        source_frame.concat(source_next_frame).concat(source_next_after_frame)
+      else
+        source_frame.concat(source_next_frame)
+      end
+    end
+  
+    def all_x_values?(source_frame)
+      source_frame.one? && source_frame.first == 'X'
+    end
 end
